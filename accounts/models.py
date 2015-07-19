a5951f9c101421e9c92964735da23b496f3f7252
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 11:12:43 -0600

    trying to fix

--- accounts/models.py
+++ accounts/models.py
@@ -21,42 +21,44 @@ from django.utils.translation import ugettext_lazy as _
 # 		return '{0} - {1}'.format(self.user, self.project)
 
 
-# class ElementalUserManager(BaseUserManager):
+class ElementalUserManager(BaseUserManager):
 
-#     def _create_user(self, email, password,
-#                      is_staff, is_superuser, **extra_fields):
-#         """
-#         Creates and saves a User with the given email and password.
-#         """
-#         now = timezone.now()
-#         if not email:
-#             raise ValueError('The given email must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email,
-#                           is_staff=is_staff, is_active=True,
-#                           is_superuser=is_superuser, last_login=now,
-#                           date_joined=now, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-#     def create_user(self, email, password=None, **extra_fields):
-#         return self._create_user(email, password, False, False,
-#                                  **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
 
-#     def create_superuser(self, email, password, **extra_fields):
-#         return self._create_user(email, password, True, True,
-#                                  **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
 
 
-# class ElementalUser(AbstractBaseUser, PermissionsMixin):
-# 	email = models.EmailField(
-# 		verbose_name='email address',
-# 		max_length=255,
-# 		unique=True,
-# 	)
-# 	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-# 	# projects = models.ManyToManyField(Project, null=True, blank=True,
-# 	# 									   through='ProjectOwnership')
+class ElementalUser(AbstractBaseUser, PermissionsMixin):
+	email = models.EmailField(
+		verbose_name='email address',
+		max_length=255,
+		unique=True,
+	)
+	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+	# projects = models.ManyToManyField(Project, null=True, blank=True,
+	# 									   through='ProjectOwnership')
 
-# 	USERNAME_FIELD = 'email'
+	objects = ElementalUserManager()
+	REQUIRED_FIELDS = []
+	USERNAME_FIELD = 'email'
