5cf634cbca96509e1b0d08ab71ac8e1385f87f2e
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 09:10:43 -0600

    fix import

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin)
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from projects.models import Project
 
--- projects/models.py
+++ projects/models.py
@@ -3,9 +3,11 @@ import re
 
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from projects.models import Project
 
 
 class Project(models.Model):
 	name = models.CharField(max_length=255)
+	date_created = models.DateTimeField(_('date created'), default=timezone.now)
