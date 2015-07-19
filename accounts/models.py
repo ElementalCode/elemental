6f190ec2a3690fb1357e29e11a3197088d5fbcea
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 10:35:27 -0600

    add user manager

--- accounts/models.py
+++ accounts/models.py
@@ -14,11 +14,39 @@ from projects.models import Project
 
 
 class ProjectOwnership(models.Model):
-	# user = models.ForeignKey('ElementalUser')
+	user = models.ForeignKey('ElementalUser')
 	project = models.ForeignKey(Project)
 
-	# def __unicode__(self):
-	# 	return '{0} - {1}'.format(self.user, self.project)
+	def __unicode__(self):
+		return '{0} - {1}'.format(self.user, self.project)
+
+
+class ElementalUserManager(BaseUserManager):
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
 
 
 class ElementalUser(AbstractBaseUser, PermissionsMixin):
@@ -28,5 +56,5 @@ class ElementalUser(AbstractBaseUser, PermissionsMixin):
 		unique=True,
 	)
 	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-	# projects = models.ManyToManyField(Project, null=True, blank=True,
-	# 									   through='ProjectOwnership')
+	projects = models.ManyToManyField(Project, null=True, blank=True,
+										   through='ProjectOwnership')
