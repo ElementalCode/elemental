c0fb457c2bc1259394e0e62b335c5e129d98cbe7
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 11:21:39 -0600

    is staff field

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group as AuthGroup
+from django.contrib.sites.models import RequestSite
+from django.core import signing
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.views.generic import TemplateView, ListView
+from django.views.generic.base import View
+from django.views.generic.edit import (FormView, UpdateView, CreateView,
+                                       DeleteView)
 
 from .models import Greeting
 
