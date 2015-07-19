00d1c7ef3e2d260214fe7e800ec8893149cc66a7
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 10:25:11 -0600

    test clash

--- accounts/models.py
+++ accounts/models.py
@@ -13,20 +13,20 @@ from django.utils.translation import ugettext_lazy as _
 from projects.models import Project
 
 
-class ProjectOwnership(models.Model):
-	user = models.ForeignKey('ElementalUser')
-	project = models.ForeignKey(Project)
+# class ProjectOwnership(models.Model):
+# 	user = models.ForeignKey('ElementalUser')
+# 	project = models.ForeignKey(Project)
 
-	def __unicode__(self):
-		return '{0} - {1}'.format(self.user, self.project)
+# 	def __unicode__(self):
+# 		return '{0} - {1}'.format(self.user, self.project)
 
 
-class ElementalUser(AbstractBaseUser, PermissionsMixin):
-	email = models.EmailField(
-		verbose_name='email address',
-		max_length=255,
-		unique=True,
-	)
-	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-	# projects = models.ManyToManyField(Project, null=True, blank=True,
-	# 									   through='ProjectOwnership')
+# class ElementalUser(AbstractBaseUser, PermissionsMixin):
+# 	email = models.EmailField(
+# 		verbose_name='email address',
+# 		max_length=255,
+# 		unique=True,
+# 	)
+# 	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+# 	# projects = models.ManyToManyField(Project, null=True, blank=True,
+# 	# 									   through='ProjectOwnership')
