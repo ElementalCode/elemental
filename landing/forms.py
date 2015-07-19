5a7a48831ac7e73aafe694b487890042d82ef3ad
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 13:59:39 -0600

    fix import

--- landing/forms.py
+++ landing/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import ElementalUser
+from accounts.models import ElementalUser
 
 
 class LoginForm(forms.Form):
