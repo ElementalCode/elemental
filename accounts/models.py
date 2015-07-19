867aed229b9676793ad54783fb6d1e69096c6c35
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 09:16:11 -0600

    fix tab/space issue

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ import re
 
 from django.contrib.auth.hashers import is_password_usable
 from django.contrib.auth.models import (
-    BaseUserManager,
-    AbstractBaseUser,
-    PermissionsMixin)
+	BaseUserManager,
+	AbstractBaseUser,
+	PermissionsMixin)
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -14,21 +14,21 @@ from projects.models import Project
 
 
 class ProjectOwnership(models.Model):
-    user = models.ForeignKey('ElementalUser')
-    project = models.ForeignKey(Project)
-    state = models.CharField(choices=STATES, default='pending', max_length=25)
+	user = models.ForeignKey('ElementalUser')
+	project = models.ForeignKey(Project)
+	state = models.CharField(choices=STATES, default='pending', max_length=25)
 
-    def __unicode__(self):
-        return '{0} - {1} - {2}'.format(self.user, self.organization,
-                                        self.state)
+	def __unicode__(self):
+		return '{0} - {1} - {2}'.format(self.user, self.organization,
+										self.state)
 
 
 class ElementalUser(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-    projects = models.ManyToManyField(Project, null=True, blank=True,
-                                           through='ProjectOwnership')
+		verbose_name='email address',
+		max_length=255,
+		unique=True,
+	)
+	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+	projects = models.ManyToManyField(Project, null=True, blank=True,
+										   through='ProjectOwnership')
