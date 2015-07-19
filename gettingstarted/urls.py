12cf87e96cf91b7ca97057b2af345356f97580a2
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 16:40:36 -0600

    oops, forgot to put in correct url

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -8,6 +8,6 @@ from landing.views import Index, SignUp
 urlpatterns = patterns('',
     url(r'^$', Index.as_view(), name='index'),
     url(r'^$', Index.as_view(), name='profile'),
-    url(r'^$', SignUp.as_view(), name='register'),
+    url(r'^register/$', SignUp.as_view(), name='register'),
     url(r'^admin/', include(admin.site.urls)),
 )
