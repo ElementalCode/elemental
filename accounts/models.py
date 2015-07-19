61e707b7521dc1f93724b72bd22aa790661580ed
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 13:36:29 -0600

    projects

--- accounts/models.py
+++ accounts/models.py
@@ -7,74 +7,86 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class ElementalUserManager(BaseUserManager):
 
-	def _create_user(self, email, password,
-					 is_staff, is_superuser, **extra_fields):
-		"""
-		Creates and saves a User with the given email and password.
-		"""
-		now = timezone.now()
-		if not email:
-			raise ValueError('The given email must be set')
-		email = self.normalize_email(email)
-		user = self.model(email=email,
-						  is_staff=is_staff, is_active=True,
-						  is_superuser=is_superuser, last_login=now,
-						  date_joined=now, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-	def create_user(self, email, password=None, **extra_fields):
-		return self._create_user(email, password, False, False,
-								 **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
 
-	def create_superuser(self, email, password, **extra_fields):
-		return self._create_user(email, password, True, True,
-								 **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class ProjectOwnership(models.Model):
+    user = models.ForeignKey('ElementalUser')
+    project = models.ForeignKey(Project)
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.user, self.project)
+
 
 class ElementalUser(AbstractBaseUser, PermissionsMixin):
-	"""
-	A fully featured User model with admin-compliant permissions that uses
-	a full-length email field as the username.
+    """
+    A fully featured User model with admin-compliant permissions that uses
+    a full-length email field as the username.
 
-	Email and password are required. Other fields are optional.
-	"""
-	email = models.EmailField(_('email address'), max_length=254, unique=True)
-	first_name = models.CharField(_('first name'), max_length=30, blank=True)
-	last_name = models.CharField(_('last name'), max_length=30, blank=True)
-	is_staff = models.BooleanField(_('staff status'), default=False,
-		help_text=_('Designates whether the user can log into this admin '
-					'site.'))
-	is_active = models.BooleanField(_('active'), default=True,
-		help_text=_('Designates whether this user should be treated as '
-					'active. Unselect this instead of deleting accounts.'))
-	date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    Email and password are required. Other fields are optional.
+    """
+    email = models.EmailField(_('email address'), max_length=254, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
-	objects = ElementalUserManager()
+    projects = models.ManyToManyField(Organization, blank=True,
+                                      through='ProjectOwnership')
 
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = []
+    objects = ElementalUserManager()
 
-	class Meta:
-		verbose_name = _('user')
-		verbose_name_plural = _('users')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
-	def get_absolute_url(self):
-		return "/users/%s/" % urlquote(self.email)
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
-	def get_full_name(self):
-		"""
-		Returns the first_name plus the last_name, with a space in between.
-		"""
-		full_name = '%s %s' % (self.first_name, self.last_name)
-		return full_name.strip()
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.email)
 
-	def get_short_name(self):
-		"Returns the short name for the user."
-		return self.first_name
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
 
-	def email_user(self, subject, message, from_email=None):
-		"""
-		Sends an email to this User.
-		"""
-		send_mail(subject, message, from_email, [self.email])
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+
+
+class Project(models.Model):
+	name = models.CharField(_('name'), max_length=50, blank=True)
+
+	def __unicode__(self):
+        return self.name
