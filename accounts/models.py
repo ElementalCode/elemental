8cb8bfb7036c3c038af80cc773dc1ffb68578c3a
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 19:05:45 -0600

    .

--- accounts/models.py
+++ accounts/models.py
@@ -47,7 +47,7 @@ class ProjectOwnership(models.Model):
 class ElementalUser(AbstractBaseUser, PermissionsMixin):
     alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', message='Only alphanumeric characters are allowed.')
 
-    # username = models.CharField(unique=True, max_length=20, validators=[alphanumeric])
+    username = models.CharField(blank=True, max_length=20, validators=[alphanumeric])
 
     email = models.EmailField(_('email address'), max_length=254, blank=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
