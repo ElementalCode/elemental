3b216b2c86ccc468af582f0485fd2d6b8bb05d6e
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-19 19:26:07 -0600

    user profile

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 from landing.views import Index, SignUp, Logout
+from accounts.views import ProfileView
 
 urlpatterns = patterns('',
     url(r'^$', Index.as_view(), name='index'),
-    url(r'^$', Index.as_view(), name='profile'),
+    url(r'users/(?P<pk>\d+)/^$', ProfileView.as_view(), name='profile'),
     url(r'^register/?$', SignUp.as_view(), name='register'),
     url(r'^logout/?$', Logout.as_view(), name='logout'),
     url(r'^admin/?', include(admin.site.urls)),
