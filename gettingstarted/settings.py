2f0d49d94393991d230787fddacba944645b885d
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-20 20:17:18 -0600

    no clue if this'll work

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 DATABASES['default'] =  dj_database_url.config()
 
 # Enable Connection Pooling (if desired)
-# DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
