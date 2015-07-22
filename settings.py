81a6307e8516bcedd428ed0f51621d49fe913a79
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-21 19:51:41 -0600

    more cleanup

--- settings.py
+++ settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing',
+    'apps.landing',
     'apps.accounts',
     'apps.projects',
 )
