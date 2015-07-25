fbcefc4f15eb872b987e7afa3b8d8299866f03d4
Firedrake969 <firedrake969@users.noreply.github.com>
2015-07-25 10:19:16 -0600

    things actually look nicer now

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -5,11 +5,4 @@ from .views import ProjectEdit
 
 urlpatterns = patterns('',
     url(r'^(?i)editor/?$', ProjectEdit.as_view(), name='create-project'),
-
-    # resource urls
-    ## js
-    url(r'^(?i)js/script\.js$', TemplateView.as_view(template_name='js/script.js')),
-
-    ## css
-    url(r'^(?i)css/style\.css$', TemplateView.as_view(template_name='css/style.css', content_type='text/css')),
 )
