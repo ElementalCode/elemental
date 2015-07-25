88cf60b989243d5d35ebc109cb5c717652b56684
Josh Pullen <JoshPullen27@gmail.com>
2015-07-25 12:25:29 -0400

    v0.0.3
    that flat tho

---
+++ ui/v0.0.3/resources/script.js
@@ -0,0 +1,24 @@
+interact('.pagePreview')
+  .resizable({
+    edges: { left: false, right: true, bottom: false, top: false }
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    //target.style.width  = event.rect.width + 'px';
+    target.style.flexBasis  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  });
