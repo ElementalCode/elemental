b9964b49fb999b7df77bb0c8cf4b05f86663c660
Firedrake969 <firedrake969@users.noreply.github.com>
2015-08-22 10:43:16 -0600

    start on file pane

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-interact('.pagePreview')
+interact('.leftSide')
   .resizable({
     edges: { left: false, right: true, bottom: false, top: false }
   })
@@ -18,6 +18,32 @@ interact('.pagePreview')
 
     target.style.webkitTransform = target.style.transform =
         'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  });
+
+interact('.filePane')
+  .resizable({
+    edges: { left: false, right: false, bottom: false, top: true }
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    // target.style.flexBasis  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+    target.parentNode.children[0].style.height = 'calc(100% - ' + target.style.height + ')';
+
+    // translate when resizing from top or left edges
+    // x += event.deltaRect.left;
+    // y += event.deltaRect.top;
+
+    // target.style.webkitTransform = target.style.transform =
+        // 'translate(' + x + 'px,' + y + 'px)';
 
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
