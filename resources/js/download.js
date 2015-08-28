be88dc649808c2110aca7ea07de48b433401e58f
Firedrake969 <firedrake969@users.noreply.github.com>
2015-08-28 14:13:10 -0600

    just make the whole json stuff one file... because it's easier anyways

--- resources/js/download.js
+++ resources/js/download.js
@@ -1,11 +1,13 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
 	var zip = new JSZip();
-	var jsonFiles = zip.folder('_elem');
+	var jsonFiles = zip.folder('.elem');
 
 	for (fileName in fileData) {
 		zip.file(fileName, json2html(fileData[fileName]));
-		jsonFiles.file(fileName.split('.')[0] + '.json', JSON.stringify(fileData[fileName]));
+		// jsonFiles.file(fileName.split('.')[0] + '.json', JSON.stringify(fileData[fileName]));
 	}
+
+	jsonFiles.file('project.json', JSON.stringify(fileData));
 
 	var content = zip.generate({type: 'blob'});
 	saveAs(content, 'project.zip');
