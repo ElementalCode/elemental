f932849056c808202c18f890ed8b8329ee9b7353
Firedrake969 <firedrake969@users.noreply.github.com>
2015-09-02 19:30:58 -0600

    rest framework for projects and project name cannot be blank

--- apps/rest/urls.py
+++ apps/rest/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from .views import ProjectDetail
+
 urlpatterns = patterns('',
     # nothing
+    url(r'^(?i)projects/project/(?P<pk>\d+)/?$', ProjectDetail.as_view(), name='project-data'),
 )
