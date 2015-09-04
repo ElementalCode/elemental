b31992675bd884ec4f6c0cc8f344c76a76d7410d
Firedrake969 <firedrake969@users.noreply.github.com>
2015-09-04 17:44:00 -0600

    yay creation works

--- apps/projects/static/js/siteIntegrations.js
+++ apps/projects/static/js/siteIntegrations.js
@@ -5,11 +5,16 @@ var SAVE_BUTTON = $('#shareMenu');
 SAVE_BUTTON.addEventListener('click', function(ev) {
 	if (ev.target.matches('#saveButton')) {
 		var request;
+		var config = {
+			headers: {
+				"X-CSRFToken": CSRF_TOKEN
+			}
+		};
 		if (NEW_PROJECT) {
 			request = axios.post('/rest/projects/project/', {
 				data: JSON.stringify(fileData),
 				name: 'test name'
-			});
+			}, config);
 		} else {
 			// request = axios.patch('/', {
 			// 	data: JSON.stringify(fileData)
