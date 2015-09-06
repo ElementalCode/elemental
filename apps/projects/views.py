063cd08c582ac77d2f2ac11f9bcd3b463c9ccf7b
Firedrake969 <firedrake969@users.noreply.github.com>
2015-09-06 08:03:22 -0600

    list view of all mystuff

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -14,7 +14,7 @@ from django.views.generic.base import View
 from django.views.generic.edit import (FormView, UpdateView, CreateView,
                                        DeleteView)
 
-from apps.accounts.mixins import UnbannedUserMixin
+from apps.accounts.mixins import UnbannedUserMixin, LoggedInRequiredMixin
 from .models import Project
 
 
@@ -22,13 +22,19 @@ class ProjectCreate(UnbannedUserMixin, TemplateView):
     template_name = 'editor.html'
 
     def get_context_data(self, **kwargs):
-        context = super(ProjectEdit, self).get_context_data(**kwargs)
+        context = super(ProjectCreate, self).get_context_data(**kwargs)
         context['new_project'] = True
         return context
 
 
-# class ProjectView(UnbannedUserMixin, TemplateView):
-#     template_name = 'template.html'
+class MyProjects(UnbannedUserMixin, LoggedInRequiredMixin, ListView):
+    template_name = 'my_projects.html'
+    model = Project
+    paginate_by = 25
+    context_object_name = 'projects'
+
+    def get_queryset(self): # add filtering later?
+        return Project.objects.filter(user=self.request.user)
 
 class ProjectEdit(UnbannedUserMixin, TemplateView):
     template_name = 'editor.html'
