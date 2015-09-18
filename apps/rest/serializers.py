5ab64ec4262a82a9a51816624d52ffc4e1df202c
Firedrake969 <firedrake969@users.noreply.github.com>
2015-09-18 16:22:03 -0600

    another test

--- apps/rest/serializers.py
+++ apps/rest/serializers.py
@@ -27,8 +27,10 @@ class ProjectSerializer(serializers.ModelSerializer):
 class ProjectCreateSerializer(serializers.ModelSerializer):
 
     def create(self, kwargs):
-        kwargs['user'] = self.context['request'].user
-        return Project.objects.create(**kwargs)
+        if self.context['request'].user.can_share_projects:
+            kwargs['user'] = self.context['request'].user
+            return Project.objects.create(**kwargs)
+        raise PermissionDenied;
 
     class Meta:
         model = Project
