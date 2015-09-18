9dc7cd455e276f79ef941600fa0ce0ceca0c186b
Firedrake969 <firedrake969@users.noreply.github.com>
2015-09-18 16:22:03 -0600

    another test

--- apps/projects/tests.py
+++ apps/projects/tests.py
@@ -15,7 +15,8 @@ class ProjectTestCases(TestCase):
     @override_settings(AUTH_USER_MODEL=settings.AUTH_USER_MODEL)
     def test_create_project(self):
         u = ElementalUser(password='password',
-                      username='username')
+                          username='username',
+                          can_share_projects=True)
         u.save()
         self.client.login(username='username', password='password')
         data = {
@@ -29,3 +30,21 @@ class ProjectTestCases(TestCase):
         except:
             project = None
         self.assertIsNotNone(project)
+
+    @override_settings(AUTH_USER_MODEL=settings.AUTH_USER_MODEL)
+    def test_create_project_fail(self):
+        u = ElementalUser(password='password',
+                          username='username')
+        u.save()
+        self.client.login(username='username', password='password')
+        data = {
+            'data': 'fake_testing_data_create_fail',
+            'name': 'my_project'
+        }
+        response = self.client.post(reverse('api:project-create'), data)
+        self.assertEqual(response.status_code, 403)
+        try:
+            project = Project.objects.get(data='fake_testing_data_create_fail')
+        except:
+            project = None
+        self.assertIsNone(project)
