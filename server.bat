de59ff738b8bb6a58f86a6d3b737612bb34d7016
Firedrake969 <Firedrake969@users.noreply.github.com>
2016-01-16 09:37:42 -0700

    Delete server.bat
    unneeded - also not everyone's going to be on Windows
    stick this in a gitignore if you'd like... but don't force it on people who won't be using it

--- server.bat
+++
@@ -1,6 +0,0 @@
-C:/Python27/python manage.py runserver 3000
-C:/Python27/python manage.py createsuperuser Dylan 123
-C:/Python27/python manage.py migrate
-C:/Python27/python manage.py makemigrations
-C:/Python27/python manage.py migrate
-pause
