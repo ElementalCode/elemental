5dba53bf6ec1642242cce6b7863f170f74a9e32b
Flame2057 <owen23355@gmail.com>
2016-06-24 16:44:23 -0400

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,7 +15,7 @@ interact('.leftSide')
     //target.style.width  = event.rect.width + 'px';
     target.style.flexBasis  = event.rect.width + 'px';
     target.style.height = event.rect.height + 'px';
-    filePane.style.width = event.rect.width - 1 + 'px';
+    filePane.style.width = event.rect.width - 2 + 'px';
 
     // translate when resizing from top or left edges
     x += event.deltaRect.left;
